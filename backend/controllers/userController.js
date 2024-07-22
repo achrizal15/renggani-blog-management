31de920b66158f4fccf0eff3924776998d67e183
ACH RIZAL <achrizal15@gmail.com>
2024-07-22 19:58:09 +0700

    get one user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,31 @@ export const createUser = async (req, res) => {
                         message: "Berhasil di create"
                 });
         } catch (error) {
-                console.log(error)
                 return res.status(500).json(error)
         }
 }
+export const getUsers = async (req, res) => {
+        try {
+                const users = await db.User.findAll({ attributes: { exclude: ['password', 'token'] } });
+                res.status(200).json({
+                        message: 'Success',
+                        data: users
+                });
+        } catch (error) {
+                return res.status(500).json(error)
+        }
+};
+export const getUserById = async (req, res) => {
+        try {
+                const id = req.params.id
+                const users = await db.User.findByPk(id,{
+                        attributes: { exclude: ['password', 'token'] }
+                });
+                res.status(200).json({
+                        message: 'Success',
+                        data: users
+                });
+        } catch (error) {
+                return res.status(500).json(error)
+        }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,12 @@ import express from 'express';
 import { verifyToken } from '../middleware/authMiddleware.js';
 import validationMiddleware from '../middleware/validationMiddleware.js'
 import { userCreateValidationRules } from '../middleware/validators/userCreateValidationRules.js'
-import { createUser } from '../controllers/userController.js'
+import { createUser, getUsers,getUserById } from '../controllers/userController.js'
 
 const router = express.Router();
 
+router.get('/users', [verifyToken], getUsers);
+router.get('/users/:id', [verifyToken], getUserById);
 router.post('/users', [verifyToken, userCreateValidationRules(), validationMiddleware], createUser);
 
 export default router;
